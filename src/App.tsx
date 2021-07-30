08910588248bc3f333bc634830497bc9be5ae109
Biprash Gautam <biprashgautam@gmail.com>
2021-07-30 23:29:24 +0545

    feat: check if user is authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+import { fetchUser } from './redux/user/creators';
+import { userSelector } from './redux/user/userSlice';
 import Routes from './routes/Routes';
 
 function App() {
+  const {user} = useAppSelector(userSelector)
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    if (!user) {
+      dispatch(fetchUser())
+    }
+  }, [user])
   return (
     <div className="min-h-screen bg-gray-100">
       <Routes />
