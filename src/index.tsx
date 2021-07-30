705601fb4927b78a3037382ac03c04e725c7428e
Biprash Gautam <biprashgautam@gmail.com>
2021-07-30 23:27:52 +0545

    feat: provide store data

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { userSlice } from './redux/user/userSlice';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {console.log(userSlice.actions, 'act')
+      }
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
