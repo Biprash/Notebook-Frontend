6b45f7d79a1f4881f015c85b3822f049e27af3f4
Biprash Gautam <biprashgautam@gmail.com>
2021-07-31 17:14:04 +0545

    fix: update css classes

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -32,7 +32,7 @@ function Login(): ReactElement {
         <>
             <Navbar />
             <div className="flex justify-center items-center h-screen">
-                <div className="rounded border-2 border-gray-300 p-8">
+                <div className="w-11/12 sm:w-7/12 md:5/12 lg:w-5/12 xl:w-4/12 mdrounded border-2 border-gray-300 p-8">
                     <h1 className="font-semibold text-3xl text-gray-800 pb-2">Login</h1>
                     <form onSubmit={handleSubmit} className="flex flex-col">
                         <label htmlFor="email" className="pt-2">Email</label>
@@ -41,7 +41,7 @@ function Login(): ReactElement {
                         <label htmlFor="password" className="pt-2">Password</label>
                         <input value={password} onChange={handlePasswordChange} className="py-1 px-2 outline-none focus:ring my-2" type="password" name="password" id="password" placeholder="password" />
 
-                        <input className="w-2/4 mx-auto text-white bg-blue-500 py-1 rounded hover:bg-blue-700" type="submit" value="Login" />
+                        <input className="w-2/4 mx-auto my-1 text-white bg-blue-500 py-1 rounded hover:bg-blue-700" type="submit" value="Login" />
                     </form>
                 
                 </div>
