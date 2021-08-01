fb37d2649e599dcd46fc0d6d35a4c691e770d69b
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-01 14:36:27 +0545

    added css

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -30,22 +30,21 @@ function Login(): ReactElement {
     
     return (
         <>
-            <Navbar />
+        <Navbar />
             <div className="flex justify-center items-center h-screen">
                 <div className="rounded border-2 border-gray-300 p-8">
                     <h1 className="font-semibold text-3xl text-gray-800 pb-2">Login</h1>
                     <form onSubmit={handleSubmit} className="flex flex-col">
                         <label htmlFor="email" className="pt-2">Email</label>
                         <input value={email} onChange={handleEmailChange} className="py-1 px-2 outline-none focus:ring my-2" type="email" name="email" id="email" placeholder="email@example.com" />
-                        
+                    
                         <label htmlFor="password" className="pt-2">Password</label>
                         <input value={password} onChange={handlePasswordChange} className="py-1 px-2 outline-none focus:ring my-2" type="password" name="password" id="password" placeholder="password" />
-
                         <input className="w-2/4 mx-auto text-white bg-blue-500 py-1 rounded hover:bg-blue-700" type="submit" value="Login" />
                     </form>
-                
                 </div>
             </div>
+        
         </>
     )
 }
