61652d6f56a7215aec3bc38b41aefd6a4d6870eb
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-01 20:59:55 +0545

    delete edit button

--- src/Components/NoteList.tsx
+++ src/Components/NoteList.tsx
@@ -15,16 +15,28 @@ interface Props {
 
 function NoteList({note}: Props): ReactElement {
     return (
-        <Link to={{ 
+        <>
+        <article className="item--card">
+                <h2 className="text-center font-bold text-lg capitalize text-gray-700 my-3">title</h2>
+                <div className="card--image bg-red-200 h-32 w-full max-w-full">
+                <Link to={{ 
             pathname:`/note/${note.id}`,
             state: { title: note.title, published:note.published }
         }} className="bg-white rounded w-40 m-2 max-h-52">
-            <img src={note.cover} alt="" className="rounded-t bg-cover bg-center bg-no-repeat" />
-            <div className="flex flex-col px-4 py-2">
-                <h2 className="font-semibold text-lg py-1">{note.title}</h2>
-                <p className="py-1">{note.description}</p>
-            </div>
+            {/* <img src={note.cover} alt="" className="rounded-t h-32 bg-no-repeat" /> */}
         </Link>
+                </div>
+                <div className="card--title">
+                    <h3 className="text-center text-lg capitalize text-gray-700 my-2">title</h3>
+                    <p className=" text-sm text-gray-500 capitalize text-left mb-8 h-16 overflow-auto" > description  </p>
+                    <hr></hr>
+                    <div className="flex justify-around mt-1 ">
+                        <Link to="#"> delete <i className="fas fa-trash-alt"></i></Link>
+                        <Link to="#"> edit <i className="fas fa-pen-nib"></i></Link>
+                    </div>
+                </div>
+            </article>
+        </>
     )
 }
 
