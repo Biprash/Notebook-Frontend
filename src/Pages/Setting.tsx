e3d1089082baaf6700e30621c5ba981a034dc475
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-03 17:14:27 +0545

    setting layout

--- src/Pages/Setting.tsx
+++ src/Pages/Setting.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
 import { Link } from 'react-router-dom';
+import '../assets/css/Style.css'
 
  import Img1 from '../assets/images/books.jpg';
 import ProfileSidebar from '../Components/ProfileSidebar'
@@ -8,27 +9,26 @@ function Setting(): ReactElement {
     <>
     <ProfileSidebar/>
         <div className="mt-16 flex flex-col justify-center items-center ">
-
-           <div className=" border-solid border-r-4 border-gray-500 rounded pr-5 ">
+           <div className=" border-solid border-r-4 border-gray-500 rounded pr-8 ">
               <h1 className="text-2xl ml-6 my-3">Account</h1>
                <div className="flex">
                    <div className=" text-white">
-                       <div className="w-52 h-28 ml-auto mr-4">
+                       <div className="w-52 h-28 ml-auto mr-6">
                            <img className="rounded-lg w-full shadow-2xl" src={Img1} alt="imgs"/>
                        </div>
                    </div>
-                   <div className=" w-1/4 text-white ">
+                   <div className=" w-1/4 ">
                      <ul>
-                        <li>
-                             <input className=" my-1 rounded border-solid border-2 border-gray-700 text-gray-800" type="text" placeholder="Name"/>
+                         <li className="setting--graduation">
+                             <input className=" my-1 ml-2 rounded border-solid border-2 border-gray-700 text-gray-800 pl-2" type="text" placeholder="University"/>
+                         </li>
+
+                         <li className="setting--envalop">
+                             <input className="my-1 ml-2 rounded border-solid border-2 border-gray-700 text-gray-800 pl-2" type="text" placeholder="Email address"/>
                          </li>
 
                          <li>
-                             <input className="my-1 rounded border-solid border-2 border-gray-700 text-gray-800" type="text" placeholder="email"/>
-                         </li>
-
-                         <li>
-                             <textarea className=" mt-1 rounded border-solid border-2 border-gray-700 text-gray-800" name="" id=""></textarea> 
+                             <textarea className=" mt-1 rounded border-solid border-2 border-gray-700 text-gray-800 pl-2" name="" id="" placeholder="Bio here"></textarea> 
                          </li>
                      </ul>
                    </div>
@@ -39,25 +39,25 @@ function Setting(): ReactElement {
                    <h1 className="text-xl mb-4">social links</h1>
                    <ul>
                        <li>
-                           <i className="mr-3 fab fa-facebook-f"></i>
-                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                           <i className="text-gray-600 mr-3 fab fa-facebook-f"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700 pl-2 " type="text" placeholder="Facebook account" />
                        </li>
 
                        <li>
-                           <i className="mr-2 fab fa-instagram"></i>
-                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                           <i className="text-gray-600 mr-2 fab fa-instagram"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700 pl-2" type="text" placeholder="Instagram account"/>
                        </li>
                        <li>
-                           <i className="mr-2 fab fa-github"></i>
-                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                           <i className="text-gray-600 mr-2 fab fa-github"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700 pl-2" type="text" placeholder="Github profile"/>
                        </li>
                        <li>
-                         <i className=" mr-2 fab fa-twitter"></i>
-                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                         <i className=" text-gray-600 mr-2 fab fa-twitter"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700 pl-2" type="text"placeholder="Twitter profile" />
                        </li>
                        <li>
-                           <i className="mr-2 fab fa-tiktok"></i>
-                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                           <i className="text-gray-600 mr-2 fab fa-tiktok"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700 pl-2" type="text" placeholder="Tiktok profile" />
                        </li>
                    </ul>
 
