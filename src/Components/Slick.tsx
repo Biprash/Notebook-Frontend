91d51b59a59090c072046e7aa3cd2b9c3835e1fe
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-03 12:18:41 +0545

    explore redegin

--- src/Components/Slick.tsx
+++ src/Components/Slick.tsx
@@ -24,39 +24,27 @@ interface Props {
 function Slick({data}: Props): ReactElement {
     
     return (
-        <div className="mt-15 border-dashed border-2 border-gray-300 py-10 px-6 w-full rounded-t-2xl my-6">
-            <h1 className="text-4xl font-bold my-6 capitalize  ">{data.title}</h1>
-
-            {/* <div className="explore"> */}
-
-            {/* <HorizontalScroll> */}
-            {/* <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5"> */}
-
-                <div className=" grid grid-cols-5 gap-8">
+        <div className=" border-dashed border-2 border-gray-300 py-10 px-6 w-full pb-52 mt-6 rounded-t-2xl">
+            <h1 className="text-4xl font-bold capitalize mb-6  ">{data.title}</h1>
+                <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 w-full h-36">
                     { data.data && data.data.map(item => {
-                    // console.log(item.cover);
                     return(
                         <Link to={{ 
                                     pathname:`/note/${item.id}`,
                                     state: { title: item.title, published: item.published, isPublic: true }
-                                    }} key={item.id} className= "border-2 bg-red-200 w-56">
-                                                
-                                <div className="l ">
-                            
-                                    <h2 className="font-bold capitalize text-lg py-1 mx-auto ">{item.title}</h2>
+                                    }} key={item.id} className= "block bg-white rounded-lg  py-8 shadow-lg">             
+                                <div className="">
+                                    <h2 className="text-center pb-2 text-xl capitalize">{item.title}</h2>
                                     <div>
-                                    <img  src={item.cover} alt="" className="rounded h-32 mt-4 w-full bg-cover bg-center bg-no-repeat" />
+                                    <img  src={item.cover} alt="" className="w-full" />
                                     </div> 
-                                    <div className=""> <p className="py-2 overflow-auto "><br/>{item.description}</p></div> 
+                                    <div className=""> <p className="truncate text-left pl-6 text-gray-600 overflow-auto "><br/>{item.description}</p></div> 
                                 </div>
                             </Link> 
                        )
                       })}
-                        {/* </div> */}   
                 </div>
-                {/* </HorizontalScroll> */}
         </div>
-        // </div>
     )
 }
 
