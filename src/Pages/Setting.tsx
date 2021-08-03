fa583d3ddb4ed4f0e8b7120462b912446bd7de00
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-03 15:55:21 +0545

    setting page

--- src/Pages/Setting.tsx
+++ src/Pages/Setting.tsx
@@ -1,49 +1,69 @@
 import { ReactElement } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-// import Img1 from '../asset/images/img1.png';
-
+ import Img1 from '../assets/images/books.jpg';
 import ProfileSidebar from '../Components/ProfileSidebar'
-
 function Setting(): ReactElement {
     return (
-        <>
-            {/* <ProfileSidebar selected="All Notes" /> */}
+    <>
+    <ProfileSidebar/>
+        <div className="mt-16 flex flex-col justify-center items-center ">
 
-            
-            <div>
-            <div className="font-mono font-medium not-italic absolute w-20 h-1.5 ml-96 mt-1.5 text-2xl flex items-center text-center text-black">account</div>
-            {/* <img className="absolute w-36 h-36 ml-96 mt-16 rounded-2xl shadow-2xl drop-shadow-3xl" src={Img1} alt="iamges"/>  */}
-            
-               <form action="">
-                <div className="absolute  ml-xl mt-20 "> </div> 
-                <input className="absolute w-52 h-6 ml-106 mt-20 border box-border rounded-sm text-gray-600" type="text" name="name" />
-              
-                <div className="absolute  ml-xl mt-28 "></div> 
-               
-                <input className="absolute w-52 h-6 ml-106 mt-28 border box-border rounded-sm text-gray-600"  type="text" name="name" />
-               
-                <textarea className="absolute w-52 h-16 ml-106 mt-36 bg-gray-300 border box-border rounded-sm text-gray-600 inline-block align-top "  name="name" />
-                <div className="absolute w-64 h-0 ml-xl  mt-56 border bg-black "></div>
-                <label className="font-mono font-normal not-italic absolute w-28 h-5 ml-96 mt-56 text-base flex items-center text-center text-black">Social Links</label>
-                
-                <input className="absolute w-52 h-6 ml-lg mt-64  border box-border rounded-sm text-gray-600" type="text" name="name" />
-                <div className="absolute  ml-sm mt-64 "></div> 
-                <div className="absolute  ml-md mt-64 "> </div> 
-                
-                <input className="absolute w-52 h-6 ml-lg mt-72 border box-border rounded-sm text-gray-600" type="text" name="name" />
-                <div className="absolute  ml-sm mt-72 "> </div> 
-                <div className="absolute  ml-md mt-72 "> </div> 
-              
-                <input className="absolute w-52 h-6 ml-lg mt-80 border box-border rounded-sm text-gray-600" type="text" name="name" />
-                <div className="absolute  ml-sm mt-80 "> </div>
-                <div className="absolute  ml-md mt-80 "></div> 
+           <div className=" border-solid border-r-4 border-gray-500 rounded pr-5 ">
+              <h1 className="text-2xl ml-6 my-3">Account</h1>
+               <div className="flex">
+                   <div className=" text-white">
+                       <div className="w-52 h-28 ml-auto mr-4">
+                           <img className="rounded-lg w-full shadow-2xl" src={Img1} alt="imgs"/>
+                       </div>
+                   </div>
+                   <div className=" w-1/4 text-white ">
+                     <ul>
+                        <li>
+                             <input className=" my-1 rounded border-solid border-2 border-gray-700 text-gray-800" type="text" placeholder="Name"/>
+                         </li>
 
-                <div className="absolute  ml-sm mt-96 "></div> 
-                <Link to='/' className="font-mono font-normal not-italic absolute w-28 h-5 ml-lg mt-96 text-base flex items-center text-center text-black">add social</Link>
+                         <li>
+                             <input className="my-1 rounded border-solid border-2 border-gray-700 text-gray-800" type="text" placeholder="email"/>
+                         </li>
 
-               </form>
-        </div>
+                         <li>
+                             <textarea className=" mt-1 rounded border-solid border-2 border-gray-700 text-gray-800" name="" id=""></textarea> 
+                         </li>
+                     </ul>
+                   </div>
+               </div>
+
+
+               <div className=" mt-6" >
+                   <h1 className="text-xl mb-4">social links</h1>
+                   <ul>
+                       <li>
+                           <i className="mr-3 fab fa-facebook-f"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                       </li>
+
+                       <li>
+                           <i className="mr-2 fab fa-instagram"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                       </li>
+                       <li>
+                           <i className="mr-2 fab fa-github"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                       </li>
+                       <li>
+                         <i className=" mr-2 fab fa-twitter"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                       </li>
+                       <li>
+                           <i className="mr-2 fab fa-tiktok"></i>
+                           <input className="my-1 rounded border-solid border-2 border-gray-700" type="text" />
+                       </li>
+                   </ul>
+
+               </div>
+           </div>
+     </div>
      </>
     )
 }
