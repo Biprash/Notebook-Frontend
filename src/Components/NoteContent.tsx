25f34dcdae0111d841db5a7840527498791bf3e1
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-04 12:40:03 +0545

    border added

--- src/Components/NoteContent.tsx
+++ src/Components/NoteContent.tsx
@@ -105,7 +105,7 @@ function NoteContent({selectedPage}: Props): ReactElement {
             {showResourceForm ? <ResourceForm sectionId={selectedSection} resources={resources} setResources={setResources} setShowResourceForm={setShowResourceForm} updatingResource={updatingResource} /> : null}
 
             <div className="p-2 flex flex-col flex-1">
-                <div className="flex flex-row flex-wrap">
+                <div className="flex flex-row flex-wrap border-b-2 border-dashed pb-2">
                     {sections.map((section, index) => {
                         return <button key={section.id} 
                         onClick={() => {
@@ -136,13 +136,12 @@ function NoteContent({selectedPage}: Props): ReactElement {
                                         <h2 className=" px-1 h-12  capitalize font-semibold text-xl  my-4">{resource.title}</h2>
                                         <div className="content h-46 w-full bg-white">
                                         <p className="p-4  overflow-y-scroll h-44 capitalize ">{resource.description}</p>
-                                        </div>
-                                       
+                                        </div>  
                                     </Link>
                                     {!location.state?.isPublic ? 
-                                    <div className="flex my-2  justify-around ">
-                                        <button onClick={e => handleResourceUpdate(e, resource)}><i className="fas fa-pencil-alt"></i></button>
-                                        <button onClick={e => handleResourceDelete(e, resource)}><i className="fas fa-trash-alt"></i></button>
+                                    <div className="flex mt-1  justify-around ">
+                                        <button className="w-1/2 h-9 hover:bg-white" onClick={e => handleResourceUpdate(e, resource)}><i className="fas fa-pencil-alt"></i></button>
+                                        <button className="w-1/2 hover:bg-white" onClick={e => handleResourceDelete(e, resource)}><i className="fas fa-trash-alt"></i></button>
                                     </div>
                                     : null }
                                 </div>
--- src/Pages/NoteDetail.tsx
+++ src/Pages/NoteDetail.tsx
@@ -61,7 +61,7 @@ function NoteDetail(): ReactElement {
     
     return (
         <div className="flex flex-col ml-64 mt-16">
-            <div className="container flex flex-column">
+            <div className="container flex border-b-2 border-dashed">
                 <h1 className="font-bold text-3xl text-gray-800 mx-auto py-2">{location.state?.title}</h1>
                 {!location.state?.isPublic ? 
                 <button onClick={handleNotePublish} className={`text-white px-8 my-2 rounded ${notePublished?'bg-red-500 hover:bg-red-500':'bg-blue-500 hover:bg-blue-600'}`}>{notePublished? 'Unpublish Note' : 'Publish Note'}</button>
