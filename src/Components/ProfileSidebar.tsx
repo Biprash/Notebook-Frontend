a3fb9bf538bea8f360176e9c91485096ff0d3927
Biprash Gautam <biprashgautam@gmail.com>
2021-08-05 09:49:34 +0545

    feat: recently viewed

--- src/Components/ProfileSidebar.tsx
+++ src/Components/ProfileSidebar.tsx
@@ -7,7 +7,7 @@ import { useAppSelector } from '../redux/hooks'
 import { userSelector } from '../redux/user/userSlice'
 
 interface Props {
-    selected?: "All Notes" | "Bookmarks"
+    selected?: "All Notes" | "Bookmarks" | "Recently Viewed"
 }
 
 function ProfileSidebar({selected}: Props): ReactElement {
@@ -40,7 +40,7 @@ function ProfileSidebar({selected}: Props): ReactElement {
                     <Link to="/note" className={`text-gray-700 py-1 px-2 my-1 rounded hover:bg-gray-100 block ${ selected === 'All Notes' ? 'bg-white' : null}`}>All Notes</Link>
                     </li>
                     <li className="my-3">
-                        <Link to="/login" className="text-gray-700 py-1 px-2 my-1 rounded hover:bg-gray-100 block">Forked Notes</Link>
+                        <Link to="/recently viewed" className={`text-gray-700 py-1 px-2 my-1 rounded hover:bg-gray-100 block ${ selected === 'Recently Viewed' ? 'bg-white' : null}`}>Recently Viewed</Link>
                     </li>
                     <li className="my-3">
                         <Link to="/bookmarks" className={`text-gray-700 py-1 px-2 my-1 rounded hover:bg-gray-100 block ${ selected === 'Bookmarks' ? 'bg-white' : null}`}>Bookmarks</Link>
