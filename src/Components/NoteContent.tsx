92768f15c599ac76f1e87e8f0fabf20a16ee4e47
Sanjiv Chaudhary <sanjivchaudhary416@gmail.com>
2021-08-07 11:41:48 +0545

    cards detail changedd

--- src/Components/NoteContent.tsx
+++ src/Components/NoteContent.tsx
@@ -131,17 +131,17 @@ function NoteContent({selectedPage}: Props): ReactElement {
                     <div className="note-content-card">
                         {resources.map((resource, index) => {
                             return (
-                                <div key={resource.id} className="contentnote--cards bg-blue-500 shadow-2xl">
+                                <div key={resource.id} className="contentnote--cards  shadow-2xl">
                                     <Link to={{pathname: resource.link}} target={"_blank"} className="flex flex-col items-center border-b-2 border-blue-200 mb-2">
-                                        <h2 className=" px-1 h-12  capitalize font-semibold text-xl my-4 text-blue-50 border-b-2 ">{resource.title}</h2>
+                                        <h2 className=" px-1 h-12  capitalize font-semibold text-xl my-4 text-gray-7000 border-b-2 border-blue-200 ">{resource.title}</h2>
                                         <div className="h-46 w-ful ">
-                                        <p className="  overflow-auto text-blue-50 h-44 capitalize italic text-sm ">{resource.description}</p>
+                                        <p className="  overflow-auto text-gray-700 h-44 capitalize italic text-sm ">{resource.description}</p>
                                         </div>  
                                     </Link>
                                     {!location.state?.isPublic ? 
                                     <div className="flex mt-1  justify-around">
-                                        <button className="" onClick={e => handleResourceUpdate(e, resource)}><i className="hover:text-gray-800 text-blue-100 fas fa-pencil-alt "></i></button>
-                                        <button className="" onClick={e => handleResourceDelete(e, resource)}><i className="hover:text-red-900 text-blue-100 fas fa-trash-alt"></i></button>
+                                        <button className="" onClick={e => handleResourceUpdate(e, resource)}><i className="hover:text-blue-800 text-gray-700 fas fa-pencil-alt "></i></button>
+                                        <button className="" onClick={e => handleResourceDelete(e, resource)}><i className="hover:text-red-900 text-gray-700 fas fa-trash-alt"></i></button>
                                     </div>
                                     : null }
                                 </div>
