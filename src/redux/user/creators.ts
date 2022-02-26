8f48566a9b2f3b7ec5d2e4b07a41e99afa6cca2a
Biprash <biprashgautam@gmail.com>
2022-02-26 20:37:54 +0545

    change error type

--- src/redux/user/creators.ts
+++ src/redux/user/creators.ts
@@ -3,24 +3,24 @@ import { AppThunk } from "../store";
 import { Login, Register } from "./types";
 import { authFail, authStart, authLogout, getUser } from "./userSlice";
 
-export const login = ({email, password}: Login): AppThunk => async (dispatch) => {
+export const login = ({ email, password }: Login): AppThunk => async (dispatch) => {
     try {
         dispatch(authStart())
-        await server.get(process.env.REACT_APP_DOMAIN+'/sanctum/csrf-cookie')
-        const response = await server.post(process.env.REACT_APP_DOMAIN+'/login', {
+        await server.get(process.env.REACT_APP_DOMAIN + '/sanctum/csrf-cookie')
+        const response = await server.post(process.env.REACT_APP_DOMAIN + '/login', {
             email: email,
             password: password
         })
         if (response.data?.two_factor === false) {
             const user = await server.get('/user/user')
             console.log(user, 'user');
-            
+
             if (user.data)
                 dispatch(getUser(user.data?.data))
         }
-    } catch (error) {
+    } catch (error: any) {
         dispatch(authFail(error?.response.data?.message))
-    } 
+    }
 }
 
 export const fetchUser = (): AppThunk => async (dispatch) => {
@@ -28,10 +28,10 @@ export const fetchUser = (): AppThunk => async (dispatch) => {
         dispatch(authStart())
         const user = await server.get('/user/user')
         console.log(user, 'user');
-        
+
         if (user?.data)
             dispatch(getUser(user.data?.data))
-    } catch (error) {
+    } catch (error: any) {
         dispatch(authFail(error?.response.data?.message))
     }
 }
@@ -39,38 +39,38 @@ export const fetchUser = (): AppThunk => async (dispatch) => {
 export const logout = (): AppThunk => async (dispatch) => {
     try {
         dispatch(authStart())
-        await server.get(process.env.REACT_APP_DOMAIN+'/sanctum/csrf-cookie')
-        const response = await server.post(process.env.REACT_APP_DOMAIN+'/logout')
+        await server.get(process.env.REACT_APP_DOMAIN + '/sanctum/csrf-cookie')
+        const response = await server.post(process.env.REACT_APP_DOMAIN + '/logout')
         console.log(response, 'res');
-        
+
         if (!response.data) {
             dispatch(authLogout())
         }
-    } catch (error) {
+    } catch (error: any) {
         dispatch(authFail(error?.response.data?.message))
-    } 
+    }
 }
 
-export const register = ({name, email, password, confirmPassword}: Register): AppThunk => async (dispatch) => {
+export const register = ({ name, email, password, confirmPassword }: Register): AppThunk => async (dispatch) => {
     try {
         dispatch(authStart())
-        await server.get(process.env.REACT_APP_DOMAIN+'/sanctum/csrf-cookie')
-        const response = await server.post(process.env.REACT_APP_DOMAIN+'/register', {
+        await server.get(process.env.REACT_APP_DOMAIN + '/sanctum/csrf-cookie')
+        const response = await server.post(process.env.REACT_APP_DOMAIN + '/register', {
             name: name,
             email: email,
             password: password,
             password_confirmation: confirmPassword
         })
         console.log(response, 'res');
-        
+
         if (response.data === "") {
             const user = await server.get('/user/user')
             console.log(user, 'user');
-            
+
             if (user.data)
                 dispatch(getUser(user.data))
         }
-    } catch (error) {        
+    } catch (error: any) {
         dispatch(authFail(error?.response.data?.message))
-    } 
+    }
 }
